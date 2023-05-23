537dfd8dff35c95fb2edf1ea6c5858214ce6da49
Naulikha <nbiketia@gmail.com>
2023-05-23 04:48:53 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
+require "pry"
 class Dog
+    #setter method to set dog name 
+ def name=(dog_name)
+    @this_dogs_name = dog_name
+ end
 
+ #getter method to display the dog name 
+ def name 
+    @this_dogs_name
+ end
 end
+
+rex = Dog.new 
+rex.name = "rex"
+rex.name
+rex.name.upcase
+binding.pry
